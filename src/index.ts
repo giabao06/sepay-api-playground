96ae6912be1f13bfbf5b736eb3aa12590f66833f
Nguyễn Phạm Gia Bảo <hello@giabao06.xyz>
2026-03-05 11:44:14 +0700

    base server

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
 import { Elysia } from "elysia";
+import { handlePayment } from "./handlers/paymentHandler";
+import { PayloadObject } from "./types/SepayApiPayload";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app =
+    new Elysia()
+        .onError(({ error }) => {
+            console.log(error.toString());
+            return {
+                status: "failed",
+                error: error
+            }
+        })
+        .get("/", () => "welcome to maimai?")
+        .post('/new-payment', handlePayment, {
+            body: PayloadObject
+        })
+        .listen(7272);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
