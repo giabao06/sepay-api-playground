bc67428a97d213800254db2d9d93e6e8c2039d58
Nguyễn Phạm Gia Bảo <hello@giabao06.xyz>
2026-03-05 15:47:09 +0700

    rudimentary authentication

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
-import { Elysia } from "elysia";
+import {Elysia, Context, status} from "elysia";
 import { handlePayment } from "./handlers/paymentHandler";
 import { PayloadObject } from "./types/SepayApiPayload";
 import * as dotenv from "dotenv";
 
 dotenv.config();
+
+if (!process.env.WEBHOOK_URL) {
+    console.error("Error: WEBHOOK_URL not set. Please configure .env.");
+    process.exit(1);
+}
+if (!process.env.PRESHARED_API_KEY) {
+    console.log("Warning: PRESHARED_API_KEY is not set, do not use in production.\n");
+}
 
 const app =
     new Elysia()
@@ -14,9 +22,24 @@ const app =
                 error: error
             }
         })
+
         .get("/", () => "welcome to maimai?")
         .post('/new-payment', handlePayment, {
-            body: PayloadObject
+            beforeHandle({ headers }) {
+                if (process.env.PRESHARED_API_KEY) {
+                    if (headers.authorization) {
+                        const api_key = headers.authorization.split("Apikey ")[1];
+                        if (api_key != process.env.PRESHARED_API_KEY) {
+                            return status(401);
+                        }
+                    } else {
+                        return status(401);
+                    }
+                } else {
+                    console.log("Skipping auth, no api key")
+                }
+            },
+            body: PayloadObject,
         })
         .listen(7272);
 
